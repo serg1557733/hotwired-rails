02490426b28597a94dcfb39f1b97fbfb5af47fba
Serhii Borshch <72445490+serg1557733@users.noreply.github.com>
2024-10-16 16:03:18 +0200

    add update method

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -1,7 +1,8 @@
 module Api
   class ImagesController < ApplicationController
     def update
-      puts params.inspect
+        @image = Image.find params[:id]
+        @image.update! title: params[:image][:title].strip
     end
   end
 end
