036945cfd0b14f536f1e3997a94ede91b723c903
Serhii Borshch <72445490+serg1557733@users.noreply.github.com>
2024-10-16 13:42:28 +0200

    using static targets

--- app/javascript/controllers/images_controller.js
+++ app/javascript/controllers/images_controller.js
@@ -1,6 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+    static targets = ['image'] //name from tag
+    connect() {
+        const title = document.createElement('p')
+        title.textContent = this.imageTarget.alt;
+        this.element.appendChild(title)
+    }
+
     copy(e) {
         console.log('click')
         window.navigator.clipboard.writeText(e.target.src)
--- app/javascript/controllers/toast_controller.js
+++ app/javascript/controllers/toast_controller.js
@@ -4,9 +4,8 @@ export default class extends Controller {
     initialize () {
         this.p = document.querySelector('.toast');
     }
-
     show({detail: {content}}) {
-        console.log('log', )
+        console.log('show called' )
         this.p.innerText = content
     }
 }
