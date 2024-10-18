e1ebd821af9f7cf78c5f3debe8b1557434a6b36e
Serhii Borshch <72445490+serg1557733@users.noreply.github.com>
2024-10-18 17:35:24 +0200

    test turbo frame

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,15 +1,19 @@
 class FoldersController < ApplicationController
   before_action :set_folder!, only: [ :edit, :update]
+
   def index
     @folders  = Folder.order created_at: :desc
   end
 
-  def edit
-
+  def edit;
   end
 
   def update
-
+    if @folder.update folder_params
+      redirect_to folders_path
+    else
+      render :edit
+    end
   end
 
   private
@@ -17,6 +21,8 @@ class FoldersController < ApplicationController
   def set_folder!
     @folder = Folder.find params[:id]
   end
-
+  def folder_params
+    params.require(:folder).permit(:title, :description)
+  end
 
 end
